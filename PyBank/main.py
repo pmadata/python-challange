09141419d96df6e6d32823b47de9045ea4cfff69
pmadata <143486132+pmadata@users.noreply.github.com>
2023-10-24 22:00:34 +1000

    Add files via upload

---
+++ PyBank/main.py
@@ -0,0 +1,42 @@
+import os
+import csv
+filename = "Resources/budget_data.csv"
+Total_Month = 0
+Profit_Loss = 0
+Profit_Change = 0
+Change_list = []
+Max_INC = ["",0]
+Max_DEC = ["",99999999999]
+
+with open(filename) as csvfile:
+    csvread = csv.reader(csvfile)  
+    csv_header = next(csvread)
+    firstrow = next(csvread)
+    Total_Month += 1
+    Profit_Change += int(firstrow[1])
+    Profit_Loss = int(firstrow[1])
+    #max_month = str (firstrow[0])
+    for row in csvread:
+        Total_Month = Total_Month + 1
+        Profit_Change = int(row[1]) - Profit_Loss
+        Profit_Loss = int(row [1])
+        Change_list += [Profit_Change]
+        #Greatest_INC = [Profit_Change]
+        #max_seen = Change_list [0]
+        
+        if Profit_Change > Max_INC[1]:
+            Max_INC[0] = row[0]
+            Max_INC[1] = Profit_Change
+            
+        if Profit_Change < Max_DEC[1]:
+            Max_DEC[0] = row[0]
+            Max_DEC[1] = Profit_Change
+                   
+    Average = sum(Change_list)/len(Change_list)
+    print ("Financial Analysis")
+    print ("---------------------------------------------")
+    print ("Total Months:", Total_Month)
+    print ("Total:", Profit_Loss)
+    print ("Average of Change:" + str(Average))
+    print ("Greatest Increase in Profits:" + str(Max_INC[0]) + "," + "("+ str(Max_INC[1])+")")
+    print ("Greatest Decrease in Profits:" + str(Max_DEC[0]) + "," + "("+ str(Max_DEC[1])+")")
